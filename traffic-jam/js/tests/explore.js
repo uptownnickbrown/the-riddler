efe2ff9ae68c4f0f343523453a8d8eebe39455b2
uptownnickbrown <uptownnickbrown@users.noreply.github.com>
2016-02-06 20:50:25 -0500

    Figured out the answer!
    With N cars there are H(N) traffic jams (or groups of cars going the
    same speed). H(N) denotes the Nth harmonic number. Started some
    write-up.

--- traffic-jam/js/tests/explore.js
+++ traffic-jam/js/tests/explore.js
@@ -85,3 +85,33 @@ var runBulkTrial = function (numCars,numTrials) {
 
 	return (results.reduce( (prev, curr) => prev + curr )) / trials;
 }
+
+var n = 1;
+var predicted = 0;
+
+console.log('n\tpredict\tresults\tdiff');
+while (n < 500) {
+	var trialResult = runBulkTrial(n,10000);
+  predicted += (1 / n);
+  roundResult = Math.floor(trialResult * 1000) /  1000;
+  roundPredict = Math.floor(predicted * 1000) /  1000;
+	console.log(n + '\t' + roundPredict + '\t' +  roundResult + '\t' + (Math.floor(1000 * Math.abs(roundPredict - roundResult)) / 1000));
+
+	n += 1;
+}
+
+// It's a harmonic mean! When you add the second car, there's a 50/50 chance that it is going
+// as fast or faster than the previous car (and doesn't add a new traffic jam) and a 50/50 chance
+// it is going slower and does cause a new jam.
+
+// When you add a 3rd car, it has a 1/3 chance of doing the same. Add a 4th? 1/4
+
+// This series (1/1 + 1/2 + 1/3 + 1/4 + ... + 1/N) is known as a harmonic series and pops up in a
+// lot of interesting places. In particular in music.
+
+// H_n	=	sum_(k=1)^(n)1/k
+
+// http://mathworld.wolfram.com/HarmonicSeries.html
+
+
+// sum_(k=1)^infty1/k
